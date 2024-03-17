d64a8e29d1436df007c87edfecec147af35b427c
zgn <fakezgn@126.com>
2024-03-17 11:41:31 +0800

    change

--- start.py
+++ start.py
@@ -1,2 +1,8 @@
 import os
+base_path = './model'
+# download repo to the base_path directory using git
+os.system('apt install git')
+os.system('apt install git-lfs')
+os.system(f'git clone https://code.openxlab.org.cn/ustbzgn/model.git {base_path}')
+os.system(f'cd {base_path} && git lfs pull')
 os.system('streamlit run web_demo.py --server.address=0.0.0.0 --server.port 7860')
--- web_demo.py
+++ web_demo.py
@@ -20,11 +20,7 @@ from openxlab.model import download
 import os
 
 base_path = './model'
-# download repo to the base_path directory using git
-os.system('apt install git')
-os.system('apt install git-lfs')
-os.system(f'git clone https://code.openxlab.org.cn/ustbzgn/model.git {base_path}')
-os.system(f'cd {base_path} && git lfs pull')
+
 
 logger = logging.get_logger(__name__)
 
