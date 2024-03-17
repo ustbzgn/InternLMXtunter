ec204ad90395da250755e6529cd6d0ec4eb56fa4
ustbzgn <84022600+ustbzgn@users.noreply.github.com>
2024-03-17 08:51:49 +0800

    Update start.py

--- start.py
+++ start.py
@@ -1,2 +1,2 @@
 import os
-os.system('streamlit run InternLM-0.2.1dev20231121/web_demo.py --server.address=127.0.0.1 --server.port 7860')
+os.system('streamlit run InternLM-0.2.1dev20231121/web_demo.py --server.address=0.0.0.0 --server.port 7860')
